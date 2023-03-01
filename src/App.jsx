0acf57616b156b357ab3604597d9812b04119b50
yeonso08 <112466398+yeonso08@users.noreply.github.com>
2023-03-02 06:37:46 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,1 @@
-import './App.css';
-import Router from './shared/Router';
-import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
-
-function App() {
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  )
-}
-
-export default App;
