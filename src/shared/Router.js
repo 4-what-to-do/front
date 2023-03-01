c82a838ddc3cf4bd626af964e7352754273f188f
yeonso08 <112466398+yeonso08@users.noreply.github.com>
2023-03-02 06:37:01 +0900

    Update Router.js

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,17 +1,1 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import Membership from "../pages/Membership";
 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="membership" element={<Membership />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default Router;
